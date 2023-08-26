3d900a3cb9129d901d7974b6e1624705261621ff
= <=>
2023-08-26 21:05:50 +0200

    Override djoser user viewset

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,9 +1,15 @@
+from django.utils import timezone
+
 from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
+from djoser.conf import settings
 from djoser.views import UserViewSet as DjoserUserViewSet
+from djoser.compat import get_user_email, get_user_email_field_name
 from drf_spectacular.utils import extend_schema, OpenApiResponse
 
+from accounts import signals
 from accounts.models import User
 from accounts.utils import is_non_admin_user
 from .filters import UserFilter
@@ -29,3 +35,66 @@ class UserViewSet(DestroyMethodNotAllowedMixin, DjoserUserViewSet):
     @action(["get", "put", "patch"], detail=False)
     def me(self, request, *args, **kwargs):
         return super().me(request, *args, **kwargs)
+
+    @action(["post"], detail=False, url_path=f"set_{User.USERNAME_FIELD}")
+    def set_username(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.request.user
+        new_username = serializer.data["new_" + User.USERNAME_FIELD]
+
+        setattr(user, User.USERNAME_FIELD, new_username)
+        email_field_name = get_user_email_field_name(user)
+        # deactivate user to reactivate it again throw email
+        if User.USERNAME_FIELD == email_field_name:
+            user.is_active = False
+            # send deactivate signal
+            signals.user_deactivated.send(
+                sender=self.__class__, user=user, request=self.request
+            )
+        user.save()
+
+        context = {"user": user}
+        to = [get_user_email(user)]
+
+        if settings.USERNAME_CHANGED_EMAIL_CONFIRMATION:
+            settings.EMAIL.username_changed_confirmation(self.request, context).send(to)
+
+        # send activation mail in case of being not activated, to guarantee that he owns this email
+        if settings.SEND_ACTIVATION_EMAIL and not user.is_active:
+            settings.EMAIL.activation(self.request, context).send(to)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(["post"], detail=False, url_path=f"reset_{User.USERNAME_FIELD}_confirm")
+    def reset_username_confirm(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        new_username = serializer.data["new_" + User.USERNAME_FIELD]
+
+        setattr(serializer.user, User.USERNAME_FIELD, new_username)
+        if hasattr(serializer.user, "last_login"):
+            serializer.user.last_login = timezone.now()
+
+        user = serializer.user
+        email_field_name = get_user_email_field_name(user)
+        # deactivate user to reactivate it again throw email
+        if User.USERNAME_FIELD == email_field_name:
+            user.is_active = False
+            # send deactivate signal
+            signals.user_deactivated.send(
+                sender=self.__class__, user=user, request=self.request
+            )
+        user.save()
+
+        context = {"user": user}
+        to = [get_user_email(user)]
+
+        if settings.USERNAME_CHANGED_EMAIL_CONFIRMATION:
+            settings.EMAIL.username_changed_confirmation(self.request, context).send(to)
+
+        # send activation mail in case of being not activated, to guarantee that he owns this email
+        if settings.SEND_ACTIVATION_EMAIL and not user.is_active:
+            settings.EMAIL.activation(self.request, context).send(to)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
