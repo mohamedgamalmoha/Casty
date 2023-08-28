59bdc10d4fbd1bf4250243dc4bde78493574a35f
= <=>
2023-08-28 14:11:42 +0200

    Add app name

--- contracts/apps.py
+++ contracts/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ContractsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contracts'
+    verbose_name = _('Contracts')
