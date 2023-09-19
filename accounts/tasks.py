55d0bdff8099eb4a63c384e92cb1d040a4698775
= <=>
2023-09-19 09:42:20 +0200

    Add logs & retry to send email function

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -6,8 +6,12 @@ from django.forms import model_to_dict
 from django.contrib.sites.shortcuts import get_current_site
 
 from celery import shared_task
+from celery.utils.log import get_task_logger
 
 from .email import URL_EMAIL_MAP
+
+
+logger = get_task_logger(__name__)
 
 
 def model_to_dict_with_meta(obj):
@@ -74,8 +78,13 @@ def process_context(context):
     return context
 
 
-@shared_task()
+# autoretry_for: If any exceptions from the list/tuple occurs during task execution, the task will be automatically retried.
+# retry_backoff: autoretries will be delayed following the rules of exponential backoff.
+# max_retries: Maximum number of retries before giving up.
+# The first retry will have a delay of 2 sec, the second is 4 sec, the third is 8 sec, the fourth is 16 sec, and so on ...
+@shared_task(autoretry_for=(Exception,), max_retries=5, retry_backoff=2)
 def send_mail(context, to):
     context = process_context(context)
     EmailClass = URL_EMAIL_MAP[context['url_name']]
     EmailClass(context=context).send(to)
+    logger.info("Email sent successfully. [To: %s, Url: %s]", to, context['url_name'])
