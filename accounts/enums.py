4fbdb6aa2e2fc7ebb2ffd369ec2aa3cca7a84cc2
= <=>
2023-09-23 16:47:09 +0200

    Fix issues at enum

--- accounts/enums.py
+++ accounts/enums.py
@@ -7,13 +7,13 @@ from django.utils.translation import gettext_lazy as _
 class RoleChoices(models.IntegerChoices):
     ADMIN = 0, _('Admin')
     MODEL = 1, _('Model')
-    DIRECTOR = 3, _('Director')
-    OTHER = 4, _('Other')
+    DIRECTOR = 2, _('Director')
+    OTHER = 3, _('Other')
 
     @classmethod
     def excluded(cls, exclude: List = None):
         exclude = exclude or []
-        return [(label, value) for label, value in cls.choices if value not in exclude]
+        return [(value, label) for value, label in cls.choices if value not in exclude]
 
     @classmethod
     def exclude_admin(cls):
